028af895958453cc80ce8d5aef877506648bd970
Štěpán Škorpil <stopka@skorpils.cz>
2017-12-09 14:22:20 +0100

    Finished replacements of Checkbox, Form and Container

---
+++ app/controls/forms/Container.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controls\Forms;
+
+use Stopka\NetteFormRenderer\Forms\IFormOptionKeys;
+
+class Container extends \Nette\Forms\Container implements IFormOptionKeys {
+    use TContainerExtension;
+
+}
--- app/controls/forms/Form.php
+++ app/controls/forms/Form.php
@@ -6,23 +6,13 @@ namespace App\Controls\Forms;
 use Minetro\Forms\reCAPTCHA\ReCaptchaField;
 use Minetro\Forms\reCAPTCHA\ReCaptchaHolder;
 use Nette\Application\ApplicationException;
-use Nette\Utils\Html;
 use Stopka\NetteFormRenderer\Forms\IFormOptionKeys;
-use Stopka\NetteFormRenderer\HtmlFormComponent;
 
 class Form extends \Nette\Application\UI\Form implements IFormOptionKeys {
+    use TContainerExtension;
 
     /** @var string */
     private $recaptcha_name;
-
-    /**
-     * @param $name string
-     * @param $label string
-     * @param $html Html
-     */
-    public function addHtml($name, $label, $html) {
-        return $this[$name] = new HtmlFormComponent($label, $html);
-    }
 
     /**
      * @param  string $name Field name
@@ -30,7 +20,7 @@ class Form extends \Nette\Application\UI\Form implements IFormOptionKeys {
      * @return ReCaptchaField
      */
     public function addReCaptcha($name = 'captcha', $label = NULL) {
-        if($this->recaptcha_name){
+        if ($this->recaptcha_name) {
             throw new ApplicationException('Form already contains captcha');
         }
         $this->recaptcha_name = $name;
